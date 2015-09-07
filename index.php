86fda7265714b90d9715dda8c2bc272b024907df
Harry McKinney <HarryPMcKinney@Gmail.com>
2015-09-07 11:43:19 -0400

    Update index.php
    add js for copyright yr

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
 <body>
   
 <?php echo("Future home for software projects of 4.0"); ?>  
+
+        <div class="copyright"> &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>
+        </div>
   
 </body>
 </html>
