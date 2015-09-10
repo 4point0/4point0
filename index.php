691c879fd829797793ef64cabfbf1d6b221cd111
Harry McKinney <HarryPMcKinney@Gmail.com>
2015-09-10 17:08:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,17 @@
   </head>
 <body>
   
+  <div class="header"></div>
+  <div class="body"></div>
+  
 <?php echo("Future home for software projects of 4.0"); ?>  
-
+  <div class="footer">
         <div class="copyright"> &copy;
             <script>
                 document.write(new Date().getFullYear())
             </script>
         </div>
+    </div>
   
 </body>
 </html>
