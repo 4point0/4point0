1383d9d9948f6e5872fc90b8f2985afeb03cf053
Harry McKinney <HarryPMcKinney@Gmail.com>
2015-09-13 18:54:58 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html ng-app="root">
+  
   <head>
     <title>4.0</title>
   </head>
+  
 <body>
   
   <div class="header"></div>
-  <div class="body"></div>
+  
+  <div class="body">
+    
+    <div ng-controller="index">{{message}}</div>
+    
+  </div>
   
 <?php echo("Future home for software projects of 4.0"); ?>  
   <div class="footer">
